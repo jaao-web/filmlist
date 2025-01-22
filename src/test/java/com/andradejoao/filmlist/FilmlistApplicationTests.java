49407addfad72202e6558328b01df6f7a735013b
Joao Andrade <joaovgarciaa@gmail.com>
2025-01-22 14:45:08 -0300

    project start

---
+++ src/test/java/com/andradejoao/filmlist/FilmlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andradejoao.filmlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
