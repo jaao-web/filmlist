6126df7e32e379f5cdb08d9eea5474f2d1a6c599
Joao Andrade <joaovgarciaa@gmail.com>
2025-01-23 18:56:25 -0300

    properties, movie, orm

---
+++ src/main/java/com/andradejoao/filmlist/Movie.java
@@ -0,0 +1,133 @@
+package com.andradejoao.filmlist;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    
+    @Column(name = "game_year")
+    private int year;
+    private String genre;
+    private String platforms;
+    private double score;
+    private String imgUrl;
+    private String shotDescription;
+    private String longDescription;
+
+
+    public Movie() {
+    }
+
+    public Movie(Long id, String title, int year, String genre, String platforms, double score, String imgUrl, 
+    String shotDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shotDescription = shotDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return this.platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public double getScore() {
+        return this.score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return this.imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShotDescription() {
+        return this.shotDescription;
+    }
+
+    public void setShotDescription(String shotDescription) {
+        this.shotDescription = shotDescription;
+    }
+
+    public String getLongDescription() {
+        return this.longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Movie)) {
+            return false;
+        }
+        Movie movie = (Movie) o;
+        return Objects.equals(id, movie.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    
+}
